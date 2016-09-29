e1977fe31d5c5d7880d7d5240868c7f5242236a4
Tommy Savolainen <tommy.savolainen@codemen.fi>
2016-09-29 11:19:03 +0300

    YPA xroad käyttämään clietin palvlua ja complete tieto organisaatiolistaan

--- src/main/java/fi/vm/kapa/rova/client/model/YpaOrganization.java
+++ src/main/java/fi/vm/kapa/rova/client/model/YpaOrganization.java
@@ -33,41 +33,49 @@ public class YpaOrganization {
     private final String name;
     private final String identifier;
     private final List<String> roles;
+    private final boolean complete;
 
     @SuppressWarnings("unused")
     private YpaOrganization() {
-        this(null, null);
+        this(null, null, true);
     }
 
-    public YpaOrganization(String identifier, String name) {
+    public YpaOrganization(String identifier, String name, boolean complete) {
         this.identifier = identifier;
         this.name = name;
         this.roles = new ArrayList<>();
+        this.complete = complete;
     }
+
     /**
      * @return name of the organization.
      */
     public String getName() {
-            return name;
-        }
+        return name;
+    }
 
     /**
      * @return organization identifier (Y-tunnus).
      */
     public String getIdentifier() {
-            return identifier;
-        }
+        return identifier;
+    }
 
     /**
      * @return Roles for the person in the organization, for example TJ, TIL, ELI, S, IS or NIMKO.
      */
     public List<String> getRoles() {
-            return roles;
+        return roles;
     }
 
-	@Override
-	public String toString() {
-		return "YpaOrganization [name=" + name + ", identifier=" + identifier + ", roles=" + roles + "]";
-	}
+    public boolean isComplete() {
+        return complete;
+    }
+
+    @Override
+    public String toString() {
+        return "YpaOrganization [name=" + name + ", identifier=" + identifier + ", roles=" + roles + ", complete=" + complete
+                + "]";
+    }
 
 }
